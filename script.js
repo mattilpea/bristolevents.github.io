714e92757aa4d26ef55c3bad72ded452dd719fae
mattilpea <peachyguyisepic@gmail.com>
2024-11-03 19:59:49 +0000

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.classList.toggle("open");  // Toggle the 'open' class to slide the sidebar in and out
-}
